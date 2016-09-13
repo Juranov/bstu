bfd0000a2e03fc96a61292eb1a5c86650115cae9
Juranov <uranov.vital@yandex.com>
2016-09-13 14:55:46 +0300

    Calculator Lab_1

---
+++ EPAM_Calc/Calculator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_Calc
+{
+    class Calculator
+    {
+        static int Main()
+        {
+            double FirstNumber = 0.0;
+            double SecondNumber = 0.0;
+            bool end = false;
+
+
+            try
+            { while (!end)
+                {
+                    Program object1 = new Program();
+                    Console.WriteLine(" Enter first number:");
+                    FirstNumber = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(" Enter second number:");
+                    SecondNumber = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(" 1 - Addition \n 2 - Subtraction \n 3 - Multiplication \n 4 - Division \n 5 - Exit");
+                    Console.Write(" Enter the transaction code:");
+                    int transactioncode = Convert.ToInt32(System.Console.ReadLine());
+                    switch (transactioncode)
+                    {
+                        case 1:
+                            Console.Write(" The result of addition:");
+                            object1.Addition(FirstNumber, SecondNumber);
+                            break;
+
+                        case 2:
+                            Console.Write(" The result of subtraction:");
+                            object1.Subtraction(FirstNumber, SecondNumber);
+                            break;
+                        case 3:
+                            Console.Write(" The result of multiplication:");
+                            object1.Multiplication(FirstNumber, SecondNumber);
+                            break;
+
+                        case 4:
+                            Console.Write(" The result of division:");
+                            object1.Division(FirstNumber, SecondNumber);
+                            break;
+                        case 5:
+                            end = true;
+                            break;
+                    }
+
+
+
+
+                }
+            }
+
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+
+            return 0;
+
+            }
+        }
+    }
---
+++ EPAM_Calc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+
+    public void Addition (double FirstNumber, double SecondNumber)   
+    {
+        Console.WriteLine("Addition: " + (FirstNumber + SecondNumber));
+    }
+
+    public void Subtraction(double FirstNumber, double SecondNumber)
+    {
+        Console.WriteLine("Substraction: " + (FirstNumber - SecondNumber));
+    }
+
+    public void Multiplication(double FirstNumber, double SecondNumber)
+    {
+        Console.WriteLine("Multiplication: " + (FirstNumber * SecondNumber));
+    }
+
+    public void Division(double FirstNumber, double SecondNumber)
+    {
+        Console.WriteLine("Division: " + (FirstNumber / SecondNumber));
+    }
+
+
+
+}
