3e7925b471d487179f579a00e1c4ee76460ec67d
Juranov <uranov.vital@yandex.com>
2016-09-20 16:45:12 +0300

    EPAM_Calculator with Unit Test

---
+++ EPAM_Calc/EPAM_Calc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+ public class Program
+{
+
+    public double Addition (double FirstNumber, double SecondNumber)   
+    {
+        return (FirstNumber + SecondNumber);
+    }
+
+    public double Subtraction(double FirstNumber, double SecondNumber)
+    {
+        return (FirstNumber - SecondNumber);
+    }
+
+    public double Multiplication(double FirstNumber, double SecondNumber)
+    {
+        return (FirstNumber * SecondNumber);
+    }
+
+    public double Division(double FirstNumber, double SecondNumber)
+    {
+        return (FirstNumber / SecondNumber);
+    }
+
+
+
+}
---
+++ EPAM_Calc/TestDemo/Class1.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EPAM_Calc;
+
+
+namespace TestDemo
+{
+    public class Class1
+    {
+        [Test]
+        public void Test2addition2()
+        {
+            Program calc = new Program();
+            Assert.AreEqual(4, calc.Addition (2,2));
+        }
+
+        [Test]
+        public void Test4addition5()
+        {
+            Program calc = new Program();
+            Assert.AreEqual(9, calc.Addition(4, 5));
+        }
+
+        [Test]
+        public void Test7addition16()
+        {
+            Program calc = new Program();
+            Assert.AreEqual(23, calc.Addition(7, 16));
+        }
+
+        [Test]
+        public void Test2subtraction2()
+        {
+            Program calc = new Program();
+            Assert.AreEqual(0, calc.Subtraction(2, 2));
+        }
+
+        [Test]
+        public void Test5subtraction7()
+        {
+            Program calc = new Program();
+            Assert.AreEqual(-2, calc.Subtraction(5, 7));
+        }
+
+        [Test]
+        public void Test16subtraction16()
+        {
+            Program calc = new Program();
+            Assert.AreEqual(0, calc.Subtraction(16, 16));
+        }
+
+        [Test]
+        public void Test5divide0()
+        {
+            Program calc = new Program();
+            Assert.AreEqual(0, calc.Division(5, 0));
+        }
+
+        [Test]
+        public void Test6divide2()
+        {
+            Program calc = new Program();
+            Assert.AreEqual(3, calc.Division(6, 2));
+        }
+
+        [Test]
+        public void Test5divide1()
+        {
+            Program calc = new Program();
+            Assert.AreEqual(5, calc.Division(5, 1));
+        }
+
+        [Test]
+        public void Test6multiplication0()
+        {
+            Program calc = new Program();
+            Assert.AreEqual(0, calc.Multiplication(6, 0));
+        }
+
+        [Test]
+        public void Test7multiplication2()
+        {
+            Program calc = new Program();
+            Assert.AreEqual(14, calc.Multiplication(7, 2));
+        }
+
+        [Test]
+        public void Test8multiplication8()
+        {
+            Program calc = new Program();
+            Assert.AreEqual(64, calc.Multiplication(8, 8));
+        }
+    }
+}
